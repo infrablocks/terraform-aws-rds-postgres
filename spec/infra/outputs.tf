385a6c391e817f6be6199fd171140db4daf54259
Mark Levi <mdayanlevi@gmail.com>
2017-06-22 13:18:23 +0100

    expose database name in output

--- spec/infra/outputs.tf
+++ spec/infra/outputs.tf
@@ -20,3 +20,7 @@ output "postgres_database_host" {
 output "postgres_database_port" {
   value = "${module.rds.postgres_database_port}"
 }
+
+output "postgres_database_name" {
+  value = "${module.rds.postgres_database_name}"
+}
--- src/output.tf
+++ src/output.tf
@@ -1,6 +1,11 @@
 output "postgres_database_host" {
   value = "${aws_db_instance.postgres_database.address}"
 }
+
+output "postgres_database_name" {
+  value = "${aws_db_instance.postgres_database.name}"
+}
+
 output "postgres_database_port" {
   value = "5432"
 }
