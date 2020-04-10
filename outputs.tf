9257e2980daf80b47db4591c9f763a8f5f19bb6e
Toby Clemson <tobyclemson@gmail.com>
2020-04-10 20:41:18 +0100

    Fix typo.

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "postgres_database_host" {
   description = "The database host."
-  value       = aws_db_instance.postgres_database.address
+  value = aws_db_instance.postgres_database.address
 }
 
 output "postgres_database_name" {
   description = "The database name."
-  value       = aws_db_instance.postgres_database.name
+  value = aws_db_instance.postgres_database.name
 }
 
 output "postgres_database_port" {
   description = "The database port."
-  value       = "5432"
+  value = "5432"
 }
 
 output "postgres_database_sg_id" {
-  decription = "Security Group id associated with database server"
-  value      = aws_security_group.postgres_database_security_group.id
+  description = "The security group ID associated with the database."
+  value = aws_security_group.postgres_database_security_group.id
 }
