1917c666c3caa1c7e679d786230a6b379b026c1d
Corin Chaplin <corin@kroo.com>
2022-04-27 14:41:00 +0100

    Fix incorrect let assignment in tests

--- spec/rds_spec.rb
+++ spec/rds_spec.rb
@@ -6,7 +6,7 @@ describe 'RDS' do
 
   let(:database_name) { vars.database_name }
   let(:storage_type) { vars.storage_type }
-  let(:storage_type) { vars.max_allocated_storage }
+  let(:max_allocated_storage) { vars.max_allocated_storage }
 
   let(:vpc_id) { output_for(:prerequisites, 'vpc_id') }
   let(:postgres_database_port) do
